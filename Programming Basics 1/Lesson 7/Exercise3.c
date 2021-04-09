2550cb7c988227f9af85dc990002504749afd453
JFortun <josemanuelfortunromero@gmail.com>
2021-04-09 12:29:43 +0200

    Programming Basics 1 - Lesson 7 - Exercise3

---
+++ Programming Basics 1/Lesson 7/Exercise3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2;
+
+    printf("Escribe dos n%cmeros enteros:\n", 163);
+    scanf("%d %d", &numero1, &numero2);
+
+    if (numero1 == numero2) {
+        printf("Los n%cmeros son iguales", 163);
+    } else if (numero1 > numero2) {
+        printf("El primer n%cmero es mayor que el segundo n%cmero", 163, 163);
+    } else {
+        printf("El segundo n%cmero es mayor que el primer n%cmero", 163, 163);
+    }
+
+    return 0;
+}
