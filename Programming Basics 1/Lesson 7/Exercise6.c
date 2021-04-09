248bed6f15026777e77dd7a99017ef87fc2df848
JFortun <josemanuelfortunromero@gmail.com>
2021-04-09 14:53:07 +0200

    Programming Basics 1 - Lesson 7 - Exercise6

---
+++ Programming Basics 1/Lesson 7/Exercise6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Escribe un n%cmero:\n", 163);
+    scanf("%d", &numero);
+
+    if (numero == 1) {
+        printf("El n%cmero introducido corresponde al mes de Enero \n", 163);
+    } else if (numero == 2) {
+        printf("El n%cmero introducido corresponde al mes de Febrero \n", 163);
+    } else if (numero == 3) {
+        printf("El n%cmero introducido corresponde al mes de Marzo \n", 163);
+    } else if (numero == 4) {
+        printf("El n%cmero introducido corresponde al mes de Abril \n", 163);
+    } else if (numero == 5) {
+        printf("El n%cmero introducido corresponde al mes de Mayo \n", 163);
+    } else if (numero == 6) {
+        printf("El n%cmero introducido corresponde al mes de Junio \n", 163);
+    } else if (numero == 7) {
+        printf("El n%cmero introducido corresponde al mes de Julio \n", 163);
+    } else if (numero == 8) {
+        printf("El n%cmero introducido corresponde al mes de Agosto \n", 163);
+    } else if (numero == 9) {
+        printf("El n%cmero introducido corresponde al mes de Septiembre \n", 163);
+    } else if (numero == 10) {
+        printf("El n%cmero introducido corresponde al mes de Octubre \n", 163);
+    } else if (numero == 11) {
+        printf("El n%cmero introducido corresponde al mes de Noviembre \n", 163);
+    } else if (numero == 12) {
+        printf("El n%cmero introducido corresponde al mes de Diciembre \n", 163);
+    }
+
+    return 0;
+}
