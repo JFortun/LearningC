b0358d4ce64011b92eaa95792dcc69f7946e40cd
JFortun <josemanuelfortunromero@gmail.com>
2021-06-06 11:51:16 +0200

    Programming Basics 2 - Lesson 1 - Exercise 10

---
+++ ProgrammingBasics2/Lesson1/Exercise10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+int contarCoincidencias(FILE *fptr, const char *word);
+
+int main() {
+    FILE *file;
+    char nombre[100];
+    char palabra[50];
+    int veces;
+
+    printf("Enter file nombre:");
+    scanf("%s", nombre);
+
+    printf("Enter palabra to search in file:");
+    scanf("%s", palabra);
+
+    file = fopen(nombre, "r");
+
+    veces = contarCoincidencias(file, palabra);
+
+    printf("'%s' se ha encontrado %d veces", palabra, veces);
+
+    fclose(file);
+
+    return 0;
+}
+
+int contarCoincidencias(FILE *fptr, const char *word) {
+    char cadena[1000];
+    char *posicion;
+
+    int aux, contador;
+
+    contador = 0;
+
+    while ((fgets(cadena, 1000, fptr)) != NULL) {
+        aux = 0;
+
+        while ((posicion = strstr(cadena + aux, word)) != NULL) {
+            aux = (posicion - cadena) + 1;
+
+            contador++;
+        }
+    }
+
+    return contador;
+}
