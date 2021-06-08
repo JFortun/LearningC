1efc1e0f1aae939c36f4723ecaadb50d57189b1e
JFortun <josemanuelfortunromero@gmail.com>
2021-06-08 21:01:31 +0200

    Programming Basics 2 - Lesson 5 - Exercise 1

---
+++ ProgrammingBasics2/Lesson5/Exercise1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct estructura {
+    int codigo;
+    char nombre[10];
+    struct estructura *siguiente;
+} nodo;
+
+nodo *nuevo();
+
+int menu();
+
+int main() {
+
+    int opcion, codigo;
+    char nombre[10];
+
+    nodo *miLista = nuevo();
+    nodo *miNodo = nuevo();
+
+    printf("Introduzca el c%cdigo:", 162);
+    scanf("%d", &codigo);
+    printf("Introduzca el nombre");
+    scanf(" %s", nombre);
+
+    miNodo->codigo = codigo;
+    strcpy(miNodo->nombre, nombre);
+    miLista = miNodo;
+
+    do {
+        opcion = menu();
+        printf("Introduzca el c%cdigo:", 162);
+        scanf("%d", &codigo);
+        printf("Introduzca el nombre");
+        scanf(" %s", nombre);
+        miNodo = nuevo();
+        miNodo->codigo = codigo;
+        strcpy(miNodo->nombre, nombre);
+        miNodo->siguiente = miLista;
+        miLista = miNodo;
+
+    } while (opcion != 2);
+
+    nodo *auxiliar = nuevo();
+    auxiliar = miLista;
+    while (auxiliar != NULL) {
+        // Procesar el nodo apuntado por el auxiliar
+        printf("C%cdigo: %3d \t Nombre: %s\n", 162, auxiliar->codigo, auxiliar->nombre);
+        // Avanzamos el puntero auxiliar
+        auxiliar = auxiliar->siguiente;
+    }
+
+    return 0;
+}
+
+nodo *nuevo() {
+    nodo *p;
+    p = (nodo *) malloc(sizeof(nodo));
+    p->siguiente = NULL;
+    return (p);
+}
+
+int menu() {
+    int opcion;
+
+    printf("\n----- Men%c -----\n", 163);
+    printf("1 - Insertar nuevo elemento en la lista\n");
+    printf("2 - Salir\n");
+    printf("Indica una opci%cn del men%c:\n", 162, 163);
+    scanf("%d", &opcion);
+
+    return opcion;
+}
